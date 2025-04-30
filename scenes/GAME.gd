ffd988a3a1ceb83ad26bb730b5b83213e786d289
Anomasie <umdieeckedenker@gmail.com>
2025-04-30 14:58:50 +0200

    added ui

--- scenes/GAME.gd
+++ scenes/GAME.gd
@@ -6,8 +6,26 @@ extends Control
 @onready var Lines = $Screen/Game/Margin/Grid/Lines
 @onready var Columns = $Screen/Game/Margin/Grid/Columns
 
+@onready var OnLabel = $Screen/CurrentGame/Score/Margin/Grid/LeftLabel
+@onready var OffLabel = $Screen/CurrentGame/Score/Margin/Grid/RightLabel
+
+@onready var SizeX = $Screen/NewGame/MainMenu/Margin/Lines/Lines/XEdit
+@onready var SizeY = $Screen/NewGame/MainMenu/Margin/Lines/Lines/YEdit
+@onready var SubmitButton = $Screen/SubmitButton
+
+var playing_as_first_player = true
+
 func _ready() -> void:
-	set_field()
+	start_game()
+
+func start_game(first_player = playing_as_first_player) -> void:
+	playing_as_first_player = first_player
+	set_field(Vector2i(SizeX.value, SizeY.value))
+	update_score()
+	if first_player:
+		GameField.empty_field()
+	else:
+		GameField.random_field()
 
 func set_children_number(Child, number, function) -> void:
 	# add or delete children
@@ -24,9 +42,28 @@ func set_children_number(Child, number, function) -> void:
 func set_field(new_size=GameField.field_size) -> void:
 	set_children_number(Lines, new_size.x, _on_lines_child_pressed)
 	set_children_number(Columns, new_size.y, _on_columns_child_pressed)
+	if new_size != GameField.field_size:
+		GameField.set_field(new_size)
+
+func update_score(new_score = GameField.get_score()) -> void:
+	OnLabel.text = str(new_score)
+	OffLabel.text = str(GameField.field_size.x * GameField.field_size.y - new_score)
 
 func _on_columns_child_pressed(i):
 	GameField.switch_column(i)
 
 func _on_lines_child_pressed(i):
 	GameField.switch_line(i)
+
+func _on_game_field_changed() -> void:
+	update_score()
+
+func _on_first_player_pressed() -> void:
+	start_game(true)
+
+func _on_second_player_pressed() -> void:
+	start_game(false)
+
+
+func _on_submit_button_pressed() -> void:
+	print("submitted!")
--- scenes/GameField.gd
+++ scenes/GameField.gd
@@ -1,13 +1,22 @@
 @tool
 extends GridContainer
 
+signal changed
+
 @export var field_size = Vector2i(10,10)
 @export var LampButton : PackedScene
 
 func _ready():
 	set_field()
 
+func get_score():
+	var sum = 0
+	for Child in self.get_children():
+		sum += int(Child.on)
+	return sum
+
 func set_field(new_size=field_size):
+	field_size = new_size
 	self.columns = new_size.x
 	# add or delete children
 	var len_children = self.get_child_count()
@@ -15,15 +24,28 @@ func set_field(new_size=field_size):
 	if len_children < len_expected:
 		for i in len_expected - len_children:
 			self.add_child(LampButton.instantiate())
-			self.get_child(-1).set_value(randf() < 0.5)
+			self.get_child(-1).pressed.connect(_on_lamp_pressed)
 	elif len_children > len_expected:
 		for i in len_children - len_expected:
 			self.get_child(-i).queue_free()
 
+func empty_field() -> void:
+	for Child in self.get_children():
+		Child.set_value(false)
+
+func random_field() -> void:
+	for Child in self.get_children():
+		Child.set_value(randf() < 0.5)
+
 func switch_column(i):
 	for j in field_size.y:
 		self.get_child(i*field_size.x + j).switch_value()
+	changed.emit()
 
 func switch_line(i):
 	for j in field_size.x:
 		self.get_child(i + field_size.x*j).switch_value()
+	changed.emit()
+
+func _on_lamp_pressed():
+	changed.emit()
